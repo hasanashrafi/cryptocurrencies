696b5805e179f879891fc195ce35ef3e26e4d63a
hasan ashrafi <92205365+hasanashrafi@users.noreply.github.com>
2024-03-01 17:54:03 +0330

    Update cryptoAPI.js

--- cryptocurrencies/js/cryptoAPI.js
+++ cryptocurrencies/js/cryptoAPI.js
@@ -1,14 +1,11 @@
 class CryptoAPI {
-
-    constructor() {
+ constructor() {
         this.apiKey = '112f189ffe9e2f216e6453be5727b991'
     }
-
     // query the api
     async queryAPI(currency, cryptocurrency) {
         // building url
         let url = `https://api.nomics.com/v1/currencies/ticker?key=${this.apiKey}&ids=${cryptocurrency}&interval=1h,1d,7d,30d&convert=${currency}`
-
         // fetch url
         const response = await fetch(url)
         // return json
@@ -17,6 +14,5 @@ class CryptoAPI {
         return {
             resultApi
         }
-
     }
 }
